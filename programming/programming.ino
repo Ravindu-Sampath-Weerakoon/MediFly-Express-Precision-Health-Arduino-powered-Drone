aa29dae04d30d8ba6af132f2cd7d117f7eb0bcc9
Ravindu Weerakoon <you@example.com>
2024-03-24 19:53:41 +0530

    Starting programming

--- programming/programming.ino
+++ programming/programming.ino
@@ -1,11 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
-  
- 
+#define NUM_MOTORS 4
+int motorPins[NUM_MOTORS] = { 6, 9, 10, 11 };
 
+void control_drone_propellers(byte speed, byte prop) {
+  byte pwm = map(speed, 0, 200, 0, 255);  // Mapping range from 0-200 to 0-255
+  // Serial.println(pwm);
+  analogWrite(motorPins[prop], pwm);
+}
+
+void setupMotors() {
+  for (byte i = 0; i < NUM_MOTORS; i++) { pinMode(motorPins[i], OUTPUT);}
+}
+
+void setup() {
+  setupMotors();
+  // Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  control_drone_propellers(0, 0);  // Example usage with a byte value
 }
